f67cabd3d19e14e584c27c1e17f9ab76817d425b
ristoarak <risto.arak@tktk.ee>
2024-04-07 11:34:15 +0300

    add Week4Loops code

---
+++ Week4Loops/Program.cs
@@ -0,0 +1,13 @@
+﻿//For-loop
+
+
+int i = 0;
+for(i =0; i<5;i++)// i++ on sama mis  i+1
+{
+  Console.WriteLine(" We dont need no education");  
+}
+Console.WriteLine(i);
+Console.WriteLine("Have nice day.");
+
+
+
---
+++ WhileLoops/Program.cs
@@ -0,0 +1,24 @@
+﻿
+Random rnd = new Random();
+int cpuRandom;
+bool loopActive = true;// bool - true/false
+
+while (loopActive)
+{ 
+    cpuRandom = rnd.Next(1,4);
+    // Console.WriteLine($"cpu has generated {cpuRandom}");
+    Console.WriteLine("Make a guess:   Enter a number 1-3");
+
+    int usernumber = Int32.Parse(Console.ReadLine());
+    if(usernumber == cpuRandom)
+    {
+        Console.WriteLine(" Congatualations, you won!");
+        // loopActive = false;
+        break;
+            }
+    else { Console.WriteLine("Oops Try again");
+    }
+}
+Console.WriteLine("Have a nice day!");
+
+
